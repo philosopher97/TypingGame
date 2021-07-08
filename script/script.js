bee68d8f2a1230e42a6ab1e0b240e3848b707622
philosopher97 <taeho0274@gmail.com>
2021-07-09 00:34:04 +0900

    20210709

--- script/script.js
+++ script/script.js
@@ -1,22 +1,36 @@
+// GUI 관련 변수 선언
 var canvas;
 var context;
 
-var x = 0;
-var y = 30;
-
-var text = "안녕";
-
-var score = 0;
-
+// 단어 리스트
 var words_1 = ["안녕", "일곱시", "샤워", "철학", "학교", "공부", "프로그래밍"];
 
+// 출현하는 단어 클래스
+class Word {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        
+        this.text = words_1[Math.floor(Math.random() * words_1.length)];
+    }
+}
+
+// 출현하는 단어 객체들
+let word1 = new Word(-100, 30);
+let word2 = new Word(-300, 200);
+let word3 = new Word(-500, 400);
+
+// 출현하는 단어 객체들을 담은 리스트
+var classList = [word1, word2, word3];
+
+// 점수 변수
+var score = 0;
+
+// 배경 이미지 생성
 var img= new Image();
 img.src="images/bg.jpg";
 
-var x2 = -400;
-var y2 = 300;
-var text2 = "일곱시"
-
+// html파일이 실행될 때 처음 로딩되는 함수
 function loaded(){
 	canvas= document.getElementById('canvas');
 	context= canvas.getContext('2d');
@@ -26,57 +40,54 @@ function loaded(){
 	setInterval(runGame,10); //1초에 100번 호출
 }
 
+// 게임을 진행하는 함수
 function runGame(){
 	moveAll(); //캐릭터 움직이기
 	drawAll(); // 이미지들 그리기
 }
 
+// 게임 요소들을 움직이는 함수(update)
 function moveAll(){
-	x += 2;
-
-    if (x >= 1020) {
-        score -= 10;
-        x = -100;
-        y = Math.floor(Math.random() * 500) + 30;
-        text = words_1[Math.floor(Math.random() * words_1.length)];
+	for (var i=0; i<classList.length; i++) {
+        classList[i].x += 2;
     }
 
-    x2 += 2;
-
-    if (x2 >= 1020) {
-        score -= 10;
-        x2 = -500;
-        y2 = Math.floor(Math.random() * 500) + 30;
-        text2 = words_1[Math.floor(Math.random() * words_1.length)];
+    for (var i=0; i<classList.length; i++) {
+        if (classList[i].x >= 1020) {
+            score -= 10;
+            classList[i].x = -100;
+            classList[i].y = Math.floor(Math.random() * 500) + 30;
+            classList[i].text = words_1[Math.floor(Math.random() * words_1.length)];
+        }
     }
-	
+
 }
+
+// 게임 요소들을 캔버스에 그리는 함수
 function drawAll(){
     context.drawImage(img,0,0);
     context.font = "30px 바탕체";
-    context.fillText(text, x, y);
-
-    context.fillText(text2, x2, y2);
+    
+    for (var i=0; i<classList.length; i++) {
+        context.fillText(classList[i].text, classList[i].x, classList[i].y);
+    }
 
     context.fillText('점수: ' + score, 880, 650);
 }
 
+// 텍스트 입력창에 단어를 입력하고 엔터를 치면 실행되는 함수
 function change() {
     var word = document.getElementById("input");
 
-    if (word.value == text) {
-        x = -100;
-        y = Math.floor(Math.random() * 500) + 30;
-        score += 10;
-        text = words_1[Math.floor(Math.random() * words_1.length)];
+    for (var i=0; i<classList.length; i++) {
+        if (word.value == classList[i].text) {
+            classList[i].x = -100;
+            classList[i].y = Math.floor(Math.random() * 500) + 30;
+            score += 10;
+            classList[i].text = words_1[Math.floor(Math.random() * words_1.length)];
+            break;
+        }
     }
 
-    else if (word.value == text2) {
-        x2 = -100;
-        y2 = Math.floor(Math.random() * 500) + 30;
-        score += 10;
-        text2 = words_1[Math.floor(Math.random() * words_1.length)];
-    }
-
-    word.value = '';
+    word.value = ''; // 텍스트 입력창은 다시 빈 상태로 돌아감
 }
